2c08c77c8297c2462423bfc95f353e0470ea14b3
dongww.kim <dongww12@gmail.com>
2024-05-31 09:16:45 +0900

    [refactor] SimilarityLength

--- Similarity/Similarity.cpp
+++ Similarity/Similarity.cpp
@@ -6,17 +6,23 @@ using namespace std;
 class Similarity {
 public:
 	Similarity(const string& str)
-		: target(str) {}
+		: target(str), targetLength(str.size()) {}
+
 	int similarityLength(const string& str) {
 		int inputLength = str.size();
-		int targetLength = target.size();
+
 		if (inputLength == targetLength) return 60;
-		if (inputLength >= 2 * targetLength
+		if (inputLength >= 2 * targetLength 
 			|| 2 * inputLength <= targetLength) return 0;
-		int smaller = (inputLength < targetLength) ?
-			inputLength : targetLength;
-		return (1 - abs(inputLength - targetLength) / smaller) * 60;
+		return (1 - abs(inputLength - targetLength) / smaller(inputLength)) * 60;
 	}
+
 private:
+	int smaller(int inputLength)
+	{
+		return (inputLength < targetLength) ? inputLength : targetLength;
+	}
+
 	string target{};
+	int targetLength{};
 };
