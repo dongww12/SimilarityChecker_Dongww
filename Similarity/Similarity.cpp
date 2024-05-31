1b2414efc1cdccb5cdd0a81d08cc29d4e0d7b344
dongww.kim <dongww12@gmail.com>
2024-05-31 09:08:31 +0900

    [feature] SimilarityLength

--- Similarity/Similarity.cpp
+++ Similarity/Similarity.cpp
@@ -1,5 +1,22 @@
 ﻿#include <iostream>
+#include <string>
+
+using namespace std;
 
 class Similarity {
-
+public:
+	Similarity(const string& str)
+		: target(str) {}
+	int similarityLength(const string& str) {
+		int inputLength = str.size();
+		int targetLength = target.size();
+		if (inputLength == targetLength) return 60;
+		if (inputLength >= 2 * targetLength
+			|| 2 * inputLength <= targetLength) return 0;
+		int smaller = (inputLength < targetLength) ?
+			inputLength : targetLength;
+		return (1 - abs(inputLength - targetLength) / smaller) * 60;
+	}
+private:
+	string target{};
 };
