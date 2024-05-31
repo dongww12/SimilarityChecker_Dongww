2bf95d02290aa6f2b0ed090eb838237f40701227
dongww.kim <dongww12@gmail.com>
2024-05-31 10:00:15 +0900

    [refactor] Fix magic number

--- Similarity/Similarity.cpp
+++ Similarity/Similarity.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+constexpr int maxScore = 60;
+
 class Similarity {
 public:
 	Similarity(const string& str)
@@ -11,16 +13,20 @@ public:
 	int similarityLength(const string& str) {
 		int inputLength = str.size();
 
-		if (inputLength == targetLength) return 60;
-		if (inputLength >= 2 * targetLength 
-			|| 2 * inputLength <= targetLength) return 0;
-		return (1 - abs(inputLength - targetLength) / smaller(inputLength)) * 60;
+		if (inputLength == targetLength) return maxScore;
+		if (isDiffGreaterThanTwice(inputLength)) return 0;
+		return (1 - abs(inputLength - targetLength) / smaller(inputLength)) * maxScore;
 	}
 
 private:
 	int smaller(int inputLength)
 	{
 		return (inputLength < targetLength) ? inputLength : targetLength;
+	}
+
+	bool isDiffGreaterThanTwice(int inputLength)
+	{
+		return inputLength >= 2 * targetLength || 2 * inputLength <= targetLength;
 	}
 
 	string target{};
